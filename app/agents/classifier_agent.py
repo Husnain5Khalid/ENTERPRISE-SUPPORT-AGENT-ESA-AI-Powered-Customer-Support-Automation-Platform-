ea5c388b96e90ef5a0dd45d635c5fd7e36a2354c
Husnain5khalid <husnainkhalidkhan6@gmail.com>
2026-08-04 14:06:55 +0300

    Classifier Agent Has been created

---
+++ app/agents/classifier_agent.py
@@ -0,0 +1,24 @@
+from langchain_core.messages import HumanMessage, SystemMessage
+from app.agents.llm import llm
+
+
+def classifier_agent(state):
+
+    prompt = """
+Classify this support ticket.
+
+Return ONLY JSON.
+
+{
+  "category":"",
+  "priority":"",
+  "sentiment":""
+}
+"""
+
+    response = llm.invoke([
+        SystemMessage(content=prompt),
+        HumanMessage(content=state["messages"][-1].content)
+    ])
+
+    return response
