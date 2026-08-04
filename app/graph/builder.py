bde460b89dea99b5cd6f1e9b4e3ee2dba110c958
Husnain5khalid <husnainkhalidkhan6@gmail.com>
2026-08-04 14:24:48 +0300

    Builder.py has been updated

--- app/graph/builder.py
+++ app/graph/builder.py
@@ -6,7 +6,7 @@ from app.graph.router import should_continue
 from app.graph.guardrail import guardrail_node
 from app.agents.support_agent import support_agent
 from app.tools.registry import TOOLS
-
+from app.agents.classifier_agent import classifier_agent
 
 # Create graph
 graph = StateGraph(SupportState)
@@ -23,7 +23,10 @@ graph.add_node("guardrail", guardrail_node)
 # -----------------------------
 # Flow
 # -----------------------------
-graph.add_edge(START, "agent")
+graph.add_node("classifier", classifier_agent)
+
+graph.add_edge(START, "classifier")
+graph.add_edge("classifier", "agent")
 
 
 graph.add_conditional_edges(
